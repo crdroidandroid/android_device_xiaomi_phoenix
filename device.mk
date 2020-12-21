40a9f8ff086c1bc754828f90e445625e85ee983d
Pranav Vashi <neobuddy89@gmail.com>
2020-12-21 14:59:24 +0530

    phoenix: Build libkeymaster_messages for goodix
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    libkeymaster_messages.vendor:64
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
