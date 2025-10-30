3163702f10c5ae795e88b29550f698f8371e09d6
Naruto-242 <Naruto242@proton.me>
2025-10-30 09:45:19 +0000

    phoenix: Commonize sepolicy
    Change-Id: I8e62c0d704f2d5925b2eca13a3e5851542af8b04

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,9 +46,6 @@ BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # (BOARD_SUPER_PARTITION_SIZE - 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# Sepolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
 # Verified boot
 BOARD_AVB_VBMETA_SYSTEM := product system system_ext
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
